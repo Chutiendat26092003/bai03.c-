22787390ddbd69fdb65ab6971ffac1b1a0794282
Chutiendat26092003 <91305197+Chutiendat26092003@users.noreply.github.com>
2021-09-29 13:57:47 +0700

    Add files via upload

---
+++ bai03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int a;
+	printf("nhap so a =");
+	scanf("%d", &a);
+	printf("Binh phuong cua so do la:%d", a*a);
+	
+	return 0;
+}
